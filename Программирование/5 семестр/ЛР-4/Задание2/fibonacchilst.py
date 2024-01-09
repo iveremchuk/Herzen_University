221af171170e705a5eecdb187fa5c7e74ee697e3
iveremchuk <99014637+iveremchuk@users.noreply.github.com>
2024-01-09 11:47:13 +0300

    Create fibonacchilst.py

---
+++ Программирование/5 семестр/ЛР-4/Задание2/fibonacchilst.py
@@ -0,0 +1,25 @@
+class FibonacciLst:
+    def __init__(self):
+        self.fibs_list = [0, 1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.fibs_list) < self.n:
+            return self.fibs_list.pop()
+        else:
+            self.fibs_list.extend(self.fib(self.n - 1) + self.fib(self.n - 2))
+            return self.fibs_list.pop()
+
+    def __getitem__(self, index):
+        if index >= 0:
+            return self.fibs_list[index]
+        else:
+            raise IndexError("Index out of range")
+
+    def fib(self, n):
+        if n < 2:
+            return n
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
