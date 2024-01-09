096f8e1bfe5cc4310aaef808c700d41545290ba5
iveremchuk <99014637+iveremchuk@users.noreply.github.com>
2024-01-09 11:48:16 +0300

    Create fib_iter.py

---
+++ Программирование/5 семестр/ЛР-4/Задание3/fib_iter.py
@@ -0,0 +1,10 @@
+from itertools import islice
+
+def fib_iter(iterable):
+    a, b = 0, 1
+    for _ in range(2, len(iterable)):
+        a, b = b, a + b
+        yield a
+
+l = list(range(14))
+print(list(islice(fib_iter(l), 0, 2))) 
